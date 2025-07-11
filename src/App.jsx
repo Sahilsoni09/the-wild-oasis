8cede1c9003922ec530f46fe49e0d3cae20854c6
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-07-11 23:49:45 +0530

    feat: add reusable UI components (Button, Input, Row) with styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">the wild oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">form</Heading>
+            <form>
+              <Input type="number" placeholder="number of guest" />
+              <Input type="number" placeholder="number of guest" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
