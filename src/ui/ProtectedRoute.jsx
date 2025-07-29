b606c771130173479a115e733d5fd3631ac6785f
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-07-29 17:19:50 +0530

    feat(auth): Implement protected routes and user session check with Supabase + React Query ProtectedRoute Setup: - Created ProtectedRoute component to guard private routes - Checks user authentication status on route load - Shows full-page spinner while verifying session - Redirects to /login if unauthenticated using React Router's avigate  Session Validation: - Added getCurrentUser API util to validate Supabase session and re-fetch user data - Session checked on every load to avoid stale login states  useUser Hook: - Built useUser custom hook using React Query - Caches user info, exposes isAuthenticated, isLoading, user - Centralized user access logic across the app React Query Cache Priming: - On successful login, used queryClient.setQueryData(['user'], user) to avoid refetch and spinner delay - Ensures immediate authenticated state in ProtectedRoute after login  UX & Edge Case Handling: - Reset login form fields on error using mutation’s onSettled - Proper conditional logic: only render app routes after session is confirmed - Gracefully handles manual token removal / expired sessions with redirect

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import Spinner from "./Spinner";
+import { useUser } from "../features/authentication/useUser";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  //1. Load the authenticated user
+  const { isPending, isAuthenticated } = useUser();
+
+  // if there is no authenticated user redirect to login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isPending) navigate("/login");
+    },
+    [isAuthenticated, isPending, navigate]
+  );
+
+  // 2. While loading, show a spinner
+  if (isPending)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
