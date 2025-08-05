b49d2637cc5caadde9c7fc8766421d8beabb32ef
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-08-05 17:54:08 +0530

    remove uploader & fix input style

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  border-radius: var(--color-grey-0);
+  border: 1px solid var(--input-border, var(--color-grey-300));
+  border-radius: var(--border-radius-sm); /* was color var earlier */
   box-shadow: var(--shadow-sm);
   padding: 0.8rem 1.2rem;
+  background: var(--input-bg);
+  color: var(--input-fg);
 `;
 
 export default Input;
